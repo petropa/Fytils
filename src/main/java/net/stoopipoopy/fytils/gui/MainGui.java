3cce725d59566b3da218c4dccc55b4a8231a9b00
petropapahadjopoulos <anotherguywithabeanie@gmail.com>
2022-06-14 23:35:06 -0700

    Text working, still getting it laid out correctly sending out commit for saving purposes

--- src/main/java/net/stoopipoopy/fytils/gui/MainGui.java
+++ src/main/java/net/stoopipoopy/fytils/gui/MainGui.java
@@ -2,11 +2,25 @@ package net.stoopipoopy.fytils.gui;
 
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
+import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
+import io.github.cottonmc.cotton.gui.widget.data.VerticalAlignment;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class MainGui extends LightweightGuiDescription {
     public MainGui(){
-        WGridPanel root = new WGridPanel();
+        WGridPanel root = new WGridPanel(10);
         setRootPanel(root);
-        root.setSize(300,200);
+        root.setSize(240,120);
+        WLabel welcomeLabel = new WLabel(new LiteralText("Welcome to Fytils!"));
+        WLabel descriptionLabel = new WLabel(new LiteralText("Fytils is the one and only utility mod for dungeonfy!"));
+        root.canResize();
+        root.add(welcomeLabel,6,1);
+        root.add(descriptionLabel,0,3,1,1);
+
     }
 }
