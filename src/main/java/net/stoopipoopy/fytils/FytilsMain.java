e9a29f3bc0f31e5325344b64c8171cdb271cadad
petropapahadjopoulos <anotherguywithabeanie@gmail.com>
2022-06-15 15:30:56 -0700

    change the MainGui panel to a WPlainPanel and not a WGridPanel

--- src/main/java/net/stoopipoopy/fytils/FytilsMain.java
+++ src/main/java/net/stoopipoopy/fytils/FytilsMain.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.minecraft.util.registry.Registry;
-//u
+
 public class FytilsMain implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
--- src/main/java/net/stoopipoopy/fytils/gui/MainGui.java
+++ src/main/java/net/stoopipoopy/fytils/gui/MainGui.java
@@ -3,6 +3,7 @@ package net.stoopipoopy.fytils.gui;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
 import io.github.cottonmc.cotton.gui.widget.WLabel;
+import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
 import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
 import io.github.cottonmc.cotton.gui.widget.data.VerticalAlignment;
 import net.minecraft.text.LiteralText;
@@ -13,13 +14,12 @@ import java.awt.*;
 
 public class MainGui extends LightweightGuiDescription {
     public MainGui(){
-        WGridPanel root = new WGridPanel(10);
+        WPlainPanel root = new WPlainPanel();
         setRootPanel(root);
         root.setSize(340,220);
         WLabel welcomeLabel = new WLabel(new LiteralText("Welcome to Fytils!"));
         WLabel descriptionLabel = new WLabel(new LiteralText("Fytils is the one and only utility mod for dungeonfy!"));
-        root.canResize();
-        root.add(welcomeLabel,340/20,1,0,0);
+        root.add(welcomeLabel,(root.getWidth()/2) - welcomeLabel.getWidth(),20);
         root.add(descriptionLabel,0,3,0,0);
 
     }
